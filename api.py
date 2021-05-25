ca88ba520c02456fb18811a7a356e06e68c072e9
tomflo <tomaszflorek1@gmail.com>
2021-05-25 11:10:44 +0200

    Added Unit tests for all functions and automated documentation of endpoints
    Testing using unittests and pytest,  documentation lists all endpoints with their description, the arguments are missing

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, abort
+from flask_selfdoc import Autodoc
 from ipaddress import ip_address
 import json
 import sqlite3
@@ -6,6 +7,7 @@ import git
 
 
 app = Flask(__name__)
+auto = Autodoc(app)
 
 def db_connection():
     conn = None
@@ -13,12 +15,20 @@ def db_connection():
         conn = sqlite3.connect("zeit-ort.db")
     except sqlite3.error as e:
         print(e)
+
+    conn.set_trace_callback(print)
     return conn
 
 
 # Get Location from IP Address
-@app.route("/api/ip_location/<string:ip_input>", methods = ["GET"])
-def get_ip_location(ip_input):
+@app.route("/api/ip_location")
+@auto.doc()
+def get_ip_location():
+    """ Get Location for the given IP Address """
+    # Get IP From the request
+    #---------------------------------------------------------- ADD CHECKING VALIDITY OF THE IP ADDRESS
+    ip_input = request.args.get('ip',None)
+
 	# Connect to the database
     conn = db_connection()
     cursor = conn.cursor()
@@ -34,40 +44,70 @@ def get_ip_location(ip_input):
     result = cursor.fetchone()
     
     location =dict(country_iso=result[0], region_iso=result[1], region_name=result[2], city_name=result[3])
+    print(location)
 
     if location is not None:
    		return jsonify(location)
 
 # Get holidays for next 7 days
-@app.route("/api/get_holidays/")
+@app.route("/api/get_holidays")
+@auto.doc()
 def get_holidays():
+    """ 
+    Get global Holidays for the given data and period 
+    """
+    date   = request.args.get('date', None)
+    period = request.args.get('period', 7)
+    period = "+{} days".format(period)
+
 	# Connect to the database
-    conn = db_connection()
+    conn   = db_connection()
     cursor = conn.cursor()
 
-    cursor = conn.execute("SELECT * FROM flat_holidays WHERE date_iso BETWEEN date('now') AND date('now','+? days')"),(period,)
+    cursor = conn.execute("SELECT * FROM flat_holidays WHERE date_iso BETWEEN date(?) AND date(?,?)",(date,date,period))
 
     result = cursor.fetchall()
 
     if result is not None:
     	return jsonify(result)
 
-@app.route("/api/get_country_holidays/<string:country>", methods = ["GET"])
-def get_country_holidays(country):
-	# Connect to the database
+@app.route("/api/get_country_holidays")
+@auto.doc()
+def get_country_holidays():
+    """
+    Get holidays per country or country region
+    """ 
+
+    date = request.args.get('date', 'now')
+
+    country  = request.args.get('country',None)
+    region   = request.args.get('region',None)
+    # Get the period from the request & convert to string for SQL
+    period   = request.args.get('period', 7)
+
+    #---------------------------------------------------------- ADD LIMIT OF DAYS TO BE QUERIED
+    period   = "+{} days".format(period)
+
+    # Connect to the database
     conn = db_connection()
     cursor = conn.cursor()
 
-    cursor = conn.execute("SELECT * FROM flat_holidays WHERE date_iso BETWEEN date('now') AND date('now','+7 days') AND country_iso=?",(country,))
+    cursor = conn.execute("SELECT * FROM flat_holidays WHERE date_iso BETWEEN date(?) AND date(?,?) AND (country_iso=? OR region_iso=?)",(date,date,period,country,region))
 
     result = cursor.fetchall()
 
+    #---------------------------------------------------------- ADD CHECK FOR NO HOLIDAYS 
+    # result = ""
     if result is not None:
     	return jsonify(result)
 
-@app.route("/api/get_ip_holiday/")
+@app.route("/api/get_ip_holidays")
+@auto.doc()
 def get_ip_holidays():
-    # Get IP From the request
+    """ 
+    Get holidays per given IP address
+    """
+    # Get IP from the request
     #---------------------------------------------------------- ADD CHECKING VALIDITY OF THE IP ADDRESS
     ip_input = request.args.get('ip',None)
     # Get the period from the request & convert to string for SQL
@@ -100,8 +140,12 @@ def get_ip_holidays():
     if result is not None:
     	return jsonify(result)
 
-@app.route("/api/get_ip_density/")
+@app.route("/api/get_ip_density")
+@auto.doc()
 def get_ip_density():
+    """
+    Get location density score for given IP address
+    """
     # Get IP From the request
     #---------------------------------------------------------- ADD CHECKING VALIDITY OF THE IP ADDRESS
     ip_input = request.args.get('ip',None)
@@ -140,8 +184,12 @@ def get_ip_density():
     if result is not None:
         return jsonify(dict_result)
 
-@app.route("/api/get_all/")
+@app.route("/api/get_all")
+@auto.doc()
 def get_all():
+    """
+    Get Location, holidays and density score for a given IP address
+    """
     # Get IP From the request
     #---------------------------------------------------------- ADD CHECKING VALIDITY OF THE IP ADDRESS
     ip_input = request.args.get('ip',None)
@@ -153,7 +201,7 @@ def get_all():
     # Connect to the database
     conn = db_connection()
     cursor = conn.cursor()
-    
+
     # Get the IP address from input
     ip_addr = ip_address(ip_input)
     # Convert IP address to INT
@@ -168,7 +216,8 @@ def get_all():
     # Convert results to a dictionary
     location =dict(country_iso=ip_result[0], region_iso=ip_result[1], region_name=ip_result[2], city_name=ip_result[3], latitude=ip_result[4], longitude=ip_result[5])
 
-    cursor = conn.execute("SELECT density FROM opencell_density WHERE latitude = ROUND(?,1) AND longitude = ROUND(?,1)",(location['latitude'],location['longitude']))
+    # Query for density score
+    cursor = conn.execute("SELECT density as density_score FROM opencell_density WHERE latitude = ROUND(?,1) AND longitude = ROUND(?,1)",(location['latitude'],location['longitude']))
 
     density_result = list(cursor.fetchone())
     density_keys = [description[0] for description in cursor.description]
@@ -176,7 +225,8 @@ def get_all():
     # Query holidays in the upcoming X days for Y country or Z region
     cursor = conn.execute("SELECT * FROM flat_holidays WHERE date_iso BETWEEN date('now') AND date('now',?) AND country_iso=? AND (all_states=1 OR region_iso=?)",(period,location['country_iso'],location['region_iso']))
 
-    holiday_result = list(cursor.fetchone())
+    holiday_result = cursor.fetchall() #     holiday_result = list(cursor.fetchone())
+    print("Holidays: ",holiday_result)
     holiday_keys = [description[0] for description in cursor.description]
 
     result,keys = ip_result + density_result + holiday_result,ip_keys + density_keys + holiday_keys
@@ -191,12 +241,15 @@ def get_all():
     if result is not None:
         return jsonify(dict_result)
 
+@app.route('/documentation')
+def documentation():
+    return auto.html()
 
 @app.route("/test")
 def get_test():
 	return "Auto Deployment is working!? Yes it is! (with Debug mode)"
 
-@app.route("/api/json/ip_location", methods = ["GET"])
+@app.route("/api/json/ip_location")
 def get_json_ip_location(ip_input):
 	json_data = flask.request.json
 
